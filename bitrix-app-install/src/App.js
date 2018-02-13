4542d3f7bc45d9e0287f2905e07cbcd586f56d1d
Evgeny Belkin <sunafajro@gmail.com>
2018-02-13 21:41:03 +0300

    добавлено приложение для установки

---
+++ bitrix-app-install/src/App.js
@@ -0,0 +1,121 @@
+/* global BX24 */
+import React, { Component } from "react";
+import { Button, notification, Switch } from "antd";
+
+class App extends Component {
+  state = {
+    leadContext: false,
+    leadCard: false
+  };
+
+  componentDidMount() {
+    BX24.init(function() {
+      // BX24.callMethod('placement.get', (result) => {
+      //   console.log(result);
+      //   if (result.error()) {
+      //     console.log('error fetching placements');
+      //   } else {
+      //     console.log(result);
+      //     const placements = result.data();
+      //     console.log(placements);
+      //   }
+      // });
+    });
+  }
+
+  handleBindPlacement = (placement, key) => {
+    BX24.callMethod(
+      "placement.bind",
+      {
+        PLACEMENT: placement,
+        HANDLER: "https://app.evgenybelkin.ru/index.php",
+        TITLE: "Регистратура",
+        DESCRIPTION: "Тестовое приложение Регистратура"
+      },
+      result => {
+        if (result.error()) {
+          notification.open({
+            duration: 2,
+            description: "При добавлении элемента произошла ошибка!"
+          });
+          this.setState({ [key]: this.setState[key] });
+        } else {
+          notification.open({
+            duration: 2,
+            description: "Элемент успешно добавлен!"
+          });
+          this.setState({ [key]: true });
+        }
+      }
+    );
+  };
+
+  handleUnbindPlacement = (placement, key) => {
+    BX24.callMethod(
+      "placement.unbind",
+      {
+        PLACEMENT: placement,
+        HANDLER: "https://app.evgenybelkin.ru/index.php"
+      },
+      result => {
+        if (result.error()) {
+          notification.open({
+            duration: 2,
+            description: "При удалении элемента произошла ошибка!"
+          });
+          this.setState({ [key]: this.setState[key] });
+        } else {
+          notification.open({
+            duration: 2,
+            description: "Элемент успешно удален!"
+          });
+          this.setState({ [key]: false });
+        }
+      }
+    );
+  };
+
+  finishInstallation = () => {
+    BX24.installFinish();
+  };
+
+  onChange = (value, key, placement) => {
+    if (value) {
+      this.handleBindPlacement(placement, key);
+    } else {
+      this.handleUnbindPlacement(placement, key);
+    }
+  };
+
+  render() {
+    const { leadContext, leadCard } = this.state;
+    return (
+      <div style={{ padding: "1em" }}>
+        <h2>Установка приложения.</h2>
+        <div style={{ marginBottom: "1em" }}>
+          <Switch
+            checked={leadContext}
+            onChange={checked =>
+              this.onChange(checked, "leadContext", "CRM_LEAD_LIST_MENU")
+            }
+          />{" "}
+          Добавить ссылку на приложение в контекстное меню лида.
+        </div>
+        <div style={{ marginBottom: "1em" }}>
+          <Switch
+            checked={leadCard}
+            onChange={checked =>
+              this.onChange(checked, "leadCard", "CRM_LEAD_DETAIL_TAB")
+            }
+          />{" "}
+          Добавить ссылку на приложение в карточку лида.
+        </div>
+        <Button type="primary" onClick={this.finishInstallation}>
+          Завершить!
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
