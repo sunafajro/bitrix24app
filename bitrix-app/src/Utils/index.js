778341bd49b51adb298b3f8026f79648e7575aa5
Evgeny Belkin <sunafajro@gmail.com>
2018-08-21 22:54:53 +0300

    убраны ненужные функции добавлены новые

--- bitrix-app/src/Utils/index.js
+++ bitrix-app/src/Utils/index.js
@@ -1,16 +1,27 @@
 import { notification } from "antd";
 
-/**
- * 
- * @param {Array} arr
- * @return {boolean}
- */
-export const isArrayNotEmpty = arr => Array.isArray(arr) && arr.length;
 export const Aux = ({ children }) => children;
-
+/**
+ * Выводит уведомление
+ * @param {string} text
+ * @returns {void}
+ */
 export const notify = text => {
-    return notification.open({
-      duration: 2,
-      description: text
-    });
-  };
+  return notification.open({
+    duration: 2,
+    description: text
+  });
+};
+
+/**
+ * Преобразует массив элементов в объект
+ * @param {Array} array
+ * @returns {Object}
+ */
+export const convertArrayToObject = array => {
+  const newObject = {};
+  array.forEach(item => {
+    newObject[item.ID] = item.NAME;
+  });
+  return newObject;
+};
